de5f63ba9e3405e1545f2e5ee1276b68ee78c2a1
EdwardJrS <edward.sierrac@outlook.com>
2024-11-06 16:05:57 -0500

    Se empieza con la solucion del ejercicio de test5g, solo logica en js y sin html

--- Extras/Otros/Test5g/test5g.js
+++ Extras/Otros/Test5g/test5g.js
@@ -9,3 +9,66 @@ una vez que se empiece a preparar, se debe cambiar el estado a EN PREPARACIÓN, 
 debe cambiar el estado a EN REPARTO y cuando sea entregado al cliente, se debe cambiar el estado a ENTREGADO. Se debe llevar registro de fecha y
  hora de cada uno de los cambios de estado.
  */
+
+ /** 
+  * un while: Crear producto, tomar pedido, consultar producto, consulta pedido
+  * 
+ */
+
+
+ let orderList = [];
+ let productList = [];
+
+ // CREAR PRODUCTOS
+ function createProducts () {
+    let newProductName = prompt("Nombre del producto");
+    let newProductPrice = parseFloat(prompt("Precio de producto"));
+
+    if (!newProductName || !newProductPrice){ // VALIDA SI LOS CAMPOS SE ENVIAN VACÍOS
+        return alert("Error, verifique los datos del producto!");
+    } else {
+        let product = { // ASIGNA LOS VALORES DE LAS PROPIEDADES DEL OBJETO
+            productName: newProductName,
+            price: parseFloat(newProductPrice)
+        }
+        productList.push(product); // AGREGA EL OBJETO A LA LISTA
+        return alert(`Producto: ${product.productName} creado!`);
+    }
+ }
+
+ function showProducts () { // MOSTRAR EL LISTADO DE PRODUCTOS
+    if (productList.length < 1) {
+        alert("LA BASE DE DATOS DE PRODUCTOS ESTÁ VACÍA!");
+    } else {
+        productList.forEach(product => { // MUESTRA LOS PRODUCTOS DE LA LISTA POR NOMBRE Y PRECIO
+            console.log(`Producto: ${product.productName} Precio: ${product.price}`);
+        });
+    }
+ }
+
+ function takeOrders () {
+    if (!orderState || !customerName || !orderDate || !productName ||  !productQuantity || !totalOrder || !customerAddress || !customerCity || !customerPhoneNumber ){
+        return console.log("Error, verifique los datos del pedido!");
+    } else {
+        return console.log(`Pedido tomado con estado: ${orderState}`);
+    }
+ }
+
+ let viewMainMenu = true;
+
+ while (viewMainMenu) { // MANEJADOR DEL MENÚ PRINCIPAL
+    console.log("---------------------------------------- \nBIENVENIDO AL CHUZÓN DE JOSELO DELIVERY \n----------------------------------------");
+    let selectOption = prompt("SELECCIONE UNA OPCIÓN \n1) Crear producto \n2) Consultar productos \n3) Tomar pedido \n4) Consutar pedidos \n0) Salir \n");
+    switch (selectOption) {
+        case "1":
+            createProducts();
+            break;
+        case "2":
+            showProducts();
+            viewMainMenu = false;
+            break;
+        case "3":
+            takeOrders();
+            break;
+    }
+ }
