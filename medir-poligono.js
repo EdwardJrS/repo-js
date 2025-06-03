477c8855ce63d1a3fb2cbf48609283fdae42b5ea
EdwardJrS <edward.sierrac@outlook.com>
2025-06-03 15:47:42 -0500

    retos mouredev

--- medir-poligono.js
+++ medir-poligono.js
@@ -6,3 +6,28 @@
  * - Imprime el cálculo del área de un polígono de cada tipo.
  */
 
+function areaPoligono (poligono) {
+  let poligonoLowerCase = poligono.toLowerCase();
+  console.log(poligonoLowerCase);
+  let base = 3;
+  let altura = 8;
+  let lado = 2;
+
+  if (poligonoLowerCase !== 'Triangulo'.toLowerCase() && poligonoLowerCase !== 'Cuadrado'.toLowerCase() && poligonoLowerCase !== 'Rectangulo'.toLowerCase() ) {
+    console.log('Eso que quieres medir no es un poligono permitido');
+  } else if (poligonoLowerCase === 'triangulo') {
+    let areaTriangulo = (base * altura) / 2;
+    return console.log(areaTriangulo);
+  } else if (poligonoLowerCase === 'cuadrado') {
+    let areaCuadrado = lado**2;
+    return console.log(areaCuadrado);
+  } else if (poligonoLowerCase === 'rectangulo') {
+    let areaRectangulo = (base * altura);
+    return console.log(areaRectangulo);
+  } 
+}
+
+areaPoligono("cuadrado");
+areaPoligono("rectangulo");
+areaPoligono("triangulo");
+areaPoligono("culo");
