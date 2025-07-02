c677869a71ff3ca45a31f47d7e8f430e05038bd7
EdwardJrS <edward.sierrac@outlook.com>
2025-07-02 14:26:46 -0500

    buenas

---
+++ binario.js
@@ -0,0 +1,13 @@
+/*
+ * Crea un programa se encargue de transformar un número
+ * decimal a binario sin utilizar funciones propias del lenguaje que lo hagan directamente.
+ */
+
+let numero = 0;
+
+function bionarioConverter (num) {
+  console.log(num / 2);
+  console.log(0 / 2);
+}
+
+bionarioConverter(2);
--- contar-palabras.js
+++ contar-palabras.js
@@ -1,4 +1,4 @@
-/*
+/* MEDIO - DIFICIL
  * Crea un programa que cuente cuantas veces se repite cada palabra
  * y que muestre el recuento final de todas ellas.
  * - Los signos de puntuación no forman parte de la palabra.
@@ -7,3 +7,21 @@
  *   lo resuelvan automáticamente.
  */
 
+let contador = 0;
+
+function contarPalabras (cadena) {
+  let array = cadena.toLowerCase().split(" ");
+  console.log(array);
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] === array[i]){
+        contador += 1;
+      }
+    }
+    console.log(`La palabra ${array[i]} aparece ${contador} veces`);
+    contador = 0;
+  }
+}
+
+contarPalabras("Habia vez una vez");
